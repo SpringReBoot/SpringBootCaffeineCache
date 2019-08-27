4f6b73acd370a1dafeb6f6a18af349b4b03e82c7
SpringReBoot <54129486+SpringReBoot@users.noreply.github.com>
2019-08-26 22:10:38 -0500

    Initial version

---
+++ src/main/java/com/springreboot/cache/caffiene/config/CacheConfig.java
@@ -0,0 +1,14 @@
+package com.springreboot.cache.caffiene.config;
+
+import org.springframework.context.annotation.Configuration;
+
+//@Configuration
+public class CacheConfig {
+	
+	/**
+	 * No special configuration is needed as Spring Boot will autoconfigure Caffeine.
+	 * Just add Caffeine dependency 
+	 * 
+	 */
+	
+}
